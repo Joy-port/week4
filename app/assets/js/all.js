436de4d1de6cbb35eafbc033562e2c8bd3a0b6bb
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-01 20:12:50 +0800

    add js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,14 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+/* index */
+/* 填表單出現alert */
+
+const formSubmit = document.querySelector(".form__submit");
+//console.log(formSubmit);
+
+formSubmit.addEventListener('click',function(e){
+  console.log("submit!");
+  alert("請稍候，將由專人為您服務！若有其他需求，也歡迎來電詢問！")
+})
